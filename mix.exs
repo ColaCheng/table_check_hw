da16c0b4cf2835691156094ab74e7016f2dfcea4
Cola Cheng <imlikedog@gmail.com>
2023-06-13 17:20:55 +0800

    Add alias for compile.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule TableCheckHw.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -15,6 +16,12 @@ defmodule TableCheckHw.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp aliases do
+    [
+      compile: ["format", "compile"]
     ]
   end
 
