248ff3bf0820ec99b6f75f97bf127f98ffdbdf76
Cola Cheng <imlikedog@gmail.com>
2023-06-15 18:12:02 +0800

    Update config/runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,16 +1,18 @@
 import Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+if config_env() == :prod do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
 
-config :table_check_hw, TableCheckHw.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  config :table_check_hw, TableCheckHw.Repo,
+    # ssl: true,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+end
 
 config :table_check_hw,
   env_name: System.get_env("ENV_NAME") || "unknown",
